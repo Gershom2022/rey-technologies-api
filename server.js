1655aa4e78c78bd107541078049c1a0b648b8a1e
Gershom2022 <monyangi813@gmail.com>
2026-07-14 14:09:03 +0300

    updated CORS

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 
 // Enable CORS for your frontend
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000',     'https://rey-technologies.vercel.app'  // Production frontend
+],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
